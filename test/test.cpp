247a31badce692c89ee2c8e97aa01bb8d62d20db
zdrvzbl <sanya2003ogorodov@gmail.com>
2023-06-27 00:45:41 +0700

    initial: test.cpp

---
+++ test/test.cpp
@@ -0,0 +1,20 @@
+#include "hw.h"
+
+#include <sstream>
+
+#include <gtest/gtest.h>
+
+// Простой тест, выводим HelloWorld в поток, сравниваем вывод с ожидаемым
+TEST(HelloWorld, Test) 
+{
+  std::ostringstream oss;
+  oss << sample::HelloWorld{};
+  ASSERT_EQ("Hello World!", oss.str());
+}
+
+// Точка входа в тестовое приложение
+int main(int argc, char **argv) 
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
